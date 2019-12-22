a90741b0eebbe10990c0a306b6cc820e8a8be59c
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2019-12-22 23:30:27 +0200

    3

--- task-2.js
+++ task-2.js
@@ -1,31 +1,31 @@
+"use strict";
+
 const calculateEngravingPrice = function(message, pricePerWord) {
+  const words = message.split(" ");
 
-    const kostmessage = message.split(' ');
+  const longmessage = words.length * pricePerWord;
 
-    const longmessage = kostmessage.length * pricePerWord;
+  return longmessage;
+};
 
-    return  longmessage;
-    
-  };
-  
-  console.log(
-    calculateEngravingPrice(
-      'Proin sociis natoque et magnis parturient montes mus',
-      10,
-    ),
-  ); // 80
-  
-  console.log(
-    calculateEngravingPrice(
-      'Proin sociis natoque et magnis parturient montes mus',
-      20,
-    ),
-  ); // 160
-  
-  console.log(
-    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-  ); // 240
-  
-  console.log(
-    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-  ); // 120
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
